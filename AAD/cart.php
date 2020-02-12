f1a23725aefb66c23eb8c8ea3d3a095a0f70f7af
Peter Ergisi <N0751891@ntu.ac.uk>
2020-02-12 23:25:24 +0000

    Added GUI updates

--- AAD/cart.php
+++ AAD/cart.php
@@ -1,3 +1,24 @@
-<?php
-    phpinfo();
-?>
+<?php include("includes/header.php") ?>
+    <link rel="stylesheet" href="styles/cart.css">
+
+    <h1 id="cartText">Shopping Cart!</h1>
+
+    <div id="cartSection">
+        <table id="cartTable">
+            <tbody id="cartBody">
+            <tr>
+                <th>Product name</th>
+                <th>Price</th>
+                <th>Quantity: </th>
+                <td><img id="deleteImg"src="images/delete.png" /></td>
+            </tr>
+            </tbody>
+        </table>
+
+
+
+    </div>
+
+    <div id="totalPriceText"><h2>Total cost: $12.57</h2></div>
+
+<?php include("includes/footer.php") ?>
