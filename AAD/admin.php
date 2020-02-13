8746f2c75dede0c37f9295625f09b559ecbe5bde
MathewCutler <43961745+MathewCutler@users.noreply.github.com>
2020-02-13 11:21:30 +0000

    Add files via upload

---
+++ AAD/admin.php
@@ -0,0 +1,33 @@
+<?php include("includes/header.php") ?>
+<?php include("includes/sidebar.php") ?>
+	<link rel="stylesheet" href="styles/admin.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script>
+	var trigger;
+	var container;
+	function loadPHP()
+	{
+		var $this = $(this);
+		//get variable name
+		var target = $this.data('target');
+		//loads page into container
+		container.load('includes/' + target + '.php');
+		console.log(target);
+	}
+	function loadVar()
+	{
+		//set trigger
+		trigger = $('#sideNav ul li a');
+		//set container
+        container = $('#adminPanel');
+        trigger.on('click',loadPHP);
+	}
+  	$(document).ready(loadVar);
+    </script>
+
+	<div id="adminPanel">
+		<?php include("includes/dashboard.php") ?>
+	</div>
+
+<?php include("includes/footer.php") ?>
+<?php include("includes/bootstrap.php") ?>
